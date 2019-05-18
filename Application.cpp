eebfa060b81101b53cd4bbd9c8764ca76946cae9
sadoffnick <denys.shcherbak.official@gmail.com>
2019-05-18 15:26:00 +0300

    Task 1 completed, all functions for Application class are done

--- Application.cpp
+++ Application.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <random>
+#include <algorithm>
 
 const int VARIANT = 3;
+
+
+int randomInt(int begin, int end) {
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<> dis(begin, end);
+    return dis(gen);
+
+}
 
 int power(int a, int b){
     if(b ==  0)
@@ -11,45 +22,139 @@ int power(int a, int b){
         return a;
     int c = power(a,b/2)*power(a,b/2);
     if(b%2 == 0)
-        return c
+        return c;
     return a*c;
 
 }
 
 class Application {
+public:
+    static int CNT_APPS;
     std::string name;
     Application* prototype;
-    vector <pair <std::string,int>> functionality; // SIZE LESS THAN 7
+    std::vector <std::pair <std::string,int>> functionality; // SIZE LESS THAN 7
     double ar = 1.0;
     double mark;
 
-    Application(std::string n, Application* p, vector <pair <std::string,int>> f, double a){
+    Application(std::string n, Application* p, std::vector <std::pair <std::string,int>> f, double a){
         name = n;
         prototype = p;
         functionality = f;
-        sort(functionality.begin(),functionality.end());
+        std::sort(functionality.begin(),functionality.end());
         ar = a;
         int mrk = 0;
         for(int i = 0, k = functionality.size(); i < k; i++){
             mrk += power((functionality[i].second),2);
         }
         mark = power(mrk, VARIANT+(int)(ar / 2));
-
+        CNT_APPS++;
     }
 };
 
+int Application::CNT_APPS = 0;
+
+Application* create_random_application(){
+    std::string n = "RandomNodeNumber" + std::to_string(Application::CNT_APPS);
+    Application* p = nullptr;
+    std::vector <std::pair <std::string, int> > f;
+    int size = randomInt(1,7);
+    for(int i = 0; i < size; i++){
+        std::pair <std::string, int> p;
+        p.first = "app" ;
+        p.first += (char)('Z' -randomInt(0,25));
+        p.first += (char)('Z' - randomInt(0,25));
+     //   std::cout << "-" << p.first << "-\n";
+
+        p.second = randomInt(1,100);
+        f.push_back(p);
+    }
+    double a = randomInt(1,5000)*1.0/1000.0;
+    return new Application(n,p,f,a);
+}
+
+double distance(Application* a, Application* b){
+    if(a->functionality.size() < b->functionality.size())
+        std::swap(a,b);
+    int i = 0;
+    double sum = 0.1;
+    for(;i < b->functionality.size(); i++){
+        sum += power((a->functionality[i].second - b->functionality[i].second),2);
+    }
+    for(;i < a->functionality.size(); i++){
+        sum += power(a->functionality[i].second,2);
+    }
+    double p = std::min(a->ar,b->ar)/5.0-3*VARIANT;
+    double res = pow((double)sum,p);
+    return -log(res);
+}
+
 double distance(Application a, Application b){
     if(a.functionality.size() < b.functionality.size())
-        swap(a,b);
+        std::swap(a,b);
     int i = 0, sum = 0;
     for(;i < b.functionality.size(); i++){
-        sum += power((a.functionality[i] - b.functionality[i]),2);
+        sum += power((a.functionality[i].second - b.functionality[i].second),2);
     }
     for(;i < a.functionality.size(); i++){
-        sum += power(a.functionality[i],2);
+        sum += power(a.functionality[i].second,2);
     }
-    return pow(sum,ar/5-3*VARIANT);
+    return pow(sum,std::min(a.ar,b.ar)/5-3*VARIANT);
 }
+
+bool operator < (Application a, Application b){
+    return a.ar < b.ar;
+}
+
+std::ostream& operator<<(std::ostream& os, Application* &p)
+{
+    os << "--------------------------------------\n{\n";
+    os << "Name: " << p->name << "\n";
+    os << "Mark: " << p->mark << "\n";
+    os << "Prototype: " << (p->prototype ? p->prototype->name : "No prototype.")  << "\n";
+    os << "Rating: " << p->ar << "\n";
+    os << "Functionality: \n";
+    for(auto i : p->functionality){
+        os << " - " << i.first << " : " << i.second << "\n";
+    }
+    os << "}\n";
+    return os;
+}
+
+void merge(std::vector <Application*> &a, int l1, int r1, int l2, int r2){
+    std::vector<Application*> b;
+    int p1 = r1, p2 = r2;
+    int cnt = r2-l1+1;
+    b.resize(cnt--);
+    while(cnt >= 0){
+        if(p2 < l2)
+            b[cnt--] = a[p1--];
+        else if(p1 < l1)
+            b[cnt--] = a[p2--];
+        else if (a[p1]->mark > a[p2]->mark)
+            b[cnt--] = a[p1--];
+        else
+            b[cnt--] = a[p2--];
+    }
+
+    for(int i = 0; i <= r2-l1; i++)
+        a[l1+i] = b[i];
+}
+
+void sort(std::vector <Application*> &a, int left, int right){
+    if(left >= right) return;
+    int middle = (left+right)>>1;
+    sort(a,left,middle);
+    sort(a,middle+1,right);
+    merge(a,left,middle,middle+1,right);
+}
+
+void print(std::vector <Application*> apps){
+    for(auto a : apps){
+        std::cout << a << std::endl;
+    }
+}
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include "Application.h"
+
+const int MAX_SIZE = 500;
+const int TEST_SIZE = 132;
+const int SORT_SIZE = 14;
 
 int main() {
-    std::cout << "Hello, Den!" << std::endl;
+    std::vector <Application*> apps;
+
+    std::cout << "Task 1:\n";
+
+    apps.resize(TEST_SIZE);
+    for(int i = 0; i < TEST_SIZE; i++){
+        apps[i] = create_random_application();
+
+    }
+    sort(apps,0,apps.size()-1);
+    print(apps);
+
+    int num_pairs = (TEST_SIZE*(TEST_SIZE-1))/2;
+    std::cout << num_pairs << std::endl;
+    std::pair <int, std::pair <int,int>> distances[num_pairs];
+
+    int id = 0;
+    for(int i = 1; i < TEST_SIZE; i++) {
+        id += i-1;
+        for(int j = 0; j < TEST_SIZE; j++) {
+            if(i > j)
+                distances[id+j] = {distance(apps[i],apps[j]),{i,j}};
+        }
+    }
+
+
+    std::sort(distances, distances+num_pairs);
+    std::cout << "The smallest distances:\n";
+    for(int i = 0; i < SORT_SIZE; i++) {
+        std::cout << "{" << distances[i].first << ", (" << distances[i].second.first << ", " << distances[i].second.second << ")}\n";
+    }
+    std::cout << "The biggest distances:\n";
+    for(int i = num_pairs-SORT_SIZE; i < num_pairs; i++) {
+        std::cout << "{" << distances[i].first << ", (" << distances[i].second.first << ", " << distances[i].second.second << ")}\n";
+    }
+
     return 0;
 }
